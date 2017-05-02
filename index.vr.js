04d64c262fcc72377dabf233e23cfe626b42bfa4
Juho Kaasalainen <juho@taiste.fi>
2017-05-02 17:00:04 +0300

    Buttons to interact.

--- index.vr.js
+++ index.vr.js
@@ -6,10 +6,19 @@ import {
   Pano,
   Text,
   View,
+  VrButton,
 } from 'react-vr';
 
 export default class pong extends React.Component {
-  render() {
+    constructor() {
+        super();
+        this.state = {
+            color: 'red',
+            station : 1
+        }
+    }
+
+    render() {
     return (
       <View>
         <Pano source={asset('mountain.jpg')}/>
@@ -25,8 +34,41 @@ export default class pong extends React.Component {
             textAlignVertical: 'center',
             transform: [{translate: [0, 0, -3]}],
           }}>
-          1 Lentoasema{"\n"}11:58
+          {this.state.station} Lentoasema{"\n"}11:58
         </Text>
+
+        <VrButton
+            style={{
+                width: 10.4,
+                height:0.4,
+                paddingTop: 0.2,
+                paddingLeft: 0.2,
+                paddingRight: 0.2,
+                borderRadius: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderStyle: 'solid',
+                borderColor: 'red',
+                borderWidth: 0.01,
+                layoutOrigin: [0.4, 0]
+            }}
+            onEnter={()=>this.setState((prevState) => ({color: 'green', station: prevState.station + 1}))}
+            onClick={()=>this.setState((prevState) => ({color: 'blue', station: prevState.station + 1}))}>
+            <Text
+                    style= {{
+                            backgroundColor: this.state.color,
+                            fontSize: 0.8,
+                            fontWeight: '400',
+                            layoutOrigin: [0.5, 0.5],
+                            paddingLeft: 0.2,
+                            paddingRight: 0.2,
+                            textAlign: 'center',
+                            textAlignVertical: 'center',
+                            transform: [{translate: [0, 0, -3]}],
+                        }}>
+                    Nappi
+            </Text>
+        </VrButton>
       </View>
     );
   }
