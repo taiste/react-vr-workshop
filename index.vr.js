ac783552b9498d66e6307daba8c3ad6e80418689
Juho Kaasalainen <juho@taiste.fi>
2017-05-03 20:46:32 +0300

    Tweaked alignment

--- index.vr.js
+++ index.vr.js
@@ -13,6 +13,10 @@ export default class pong extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            buttons: [
+                {stop: 1, color: "#242424af"},
+                {stop: 69, color: "#701024af"},
+                ],
             station : 1,
             stationInfo: [],
         }
@@ -20,13 +24,14 @@ export default class pong extends React.Component {
         this.convertUnixTimeToDate = this.convertUnixTimeToDate.bind(this);
         this.fetchData = this.fetchData.bind(this);
         this.createResults = this.createResults.bind(this);
+        this.createButtons = this.createButtons.bind(this);
     }
 
     convertUnixTimeToDate(unixTime) {
         console.log(unixTime);
         var a = new Date(unixTime * 1000);
-        var hour = a.getHours();
-        var min = a.getMinutes();
+        var hour = (a.getHours() < 10 ? '0' : '') + a.getHours();
+        var min = (a.getMinutes() < 10 ? '0' : '') + a.getMinutes();
         var time = hour + ':' + min;
         console.log(time);
         return time;
@@ -50,70 +55,77 @@ export default class pong extends React.Component {
         const resultViews = [];
         for(var i = 0; i < this.state.stationInfo.length; i++)
         {
+                if(i > 4)
+                {
+                        break;
+                }
                 resultViews.push(<Text
                               style={{
                                         transform: [{translate: [1.2, 0, 0]}, {rotateY: -30}],
                                         backgroundColor: '#242424af',
-                                        textAlign: 'center',
                                         marginTop: 0.01,
                               }}>
                               {this.state.stationInfo.length > 0 ?
                               this.convertUnixTimeToDate(this.state.stationInfo[i].expecteddeparturetime) : ""}{" "}
                               {this.state.stationInfo.length > 0 ? this.state.stationInfo[i].destinationdisplay : ""}
                         </Text>);
-                if(i > 4)
-                {
-                        break;
-                }
+
         }
         return resultViews
     }
 
-    render() {
-    return (
-        <View>
-                <Pano source={asset('background.jpg')}/>
-                <View style={{
-                        transform: [{translate: [0, 0, -1]}],
-                        width: 1,
-                        layoutOrigin: [0.5, 0.5],
-                }}>
-                        <View
-                                style={{
-                                }}>
-                                <VrButton
-                                        style={{
-                                                marginTop: 0.1
-                                        }}
-                                        onClick={()=>this.fetchData(1)}>
-                                        <Text
-                                                style= {{
-                                                        backgroundColor: '#af1024af',
-                                                        textAlign: 'center',
-                                                }}>
-                                                Pysäkki 1
-                                        </Text>
-                                </VrButton>
-                                <VrButton
-                                        style={{
-                                                marginTop: 0.1
-                                        }}
-                                        onClick={()=>this.fetchData(69)}>
-                                        <Text
-                                                style= {{
-                                                       textAlign: 'center',
-                                                       backgroundColor: '#701024af',
-                                                }}>
-                                                Pysäkki 69
-                                        </Text>
-                                </VrButton>
-                        </View>
+        createButtons() {
+                const buttonViews = [];
+                for(var i = 0; i < 2; i++)
+                {
+                        buttonViews.push(
 
-                        {this.createResults()}
-                </View>
-      </View>
-    );
-  }
+                        );
+                }
+                return buttonViews;
+        }
+
+        render() {
+                return (
+                        <View>
+                                <Pano source={asset('background.jpg')}/>
+                                <View style={{
+                                        transform: [{translate: [0, 0, -1]}],
+                                        width: 1,
+                                        layoutOrigin: [0.5, 0.5],
+                                }}>
+                                        <VrButton
+                                                style={{
+                                                        marginTop: 0.1
+                                                }}
+                                                onClick={()=>this.fetchData(1)}>
+                                                <Text
+                                                        style= {{
+                                                                textAlign: 'center',
+                                                                backgroundColor: this.state.buttons[0].color,
+                                                        }}>
+                                                        {this.state.buttons[0].stop}
+                                                </Text>
+                                        </VrButton>
+                                        <VrButton
+                                                style={{
+                                                        marginTop: 0.1
+                                                }}
+                                                onClick={()=>this.fetchData(69)}>
+                                                <Text
+                                                        style= {{
+                                                                textAlign: 'center',
+                                                                backgroundColor: this.state.buttons[1].color,
+                                                        }}>
+                                                        {this.state.buttons[1].stop}
+                                                </Text>
+                                        </VrButton>
+
+                                        {this.createResults()}
+                                </View>
+                        </View>
+                );
+        }
 };
 
 AppRegistry.registerComponent('pong', () => pong);
