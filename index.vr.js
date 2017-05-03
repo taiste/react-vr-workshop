831f7ce7bb696ad656115f892d2794f128d46ba5
Juho Kaasalainen <juho@taiste.fi>
2017-05-03 21:25:34 +0300

    Refactoring indeting and obsolete code

--- index.vr.js
+++ index.vr.js
@@ -13,10 +13,6 @@ export default class pong extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            buttons: [
-                {stop: 1, color: "#242424af"},
-                {stop: 69, color: "#701024af"},
-                ],
             station : 1,
             stationInfo: [],
         }
@@ -24,7 +20,6 @@ export default class pong extends React.Component {
         this.convertUnixTimeToDate = this.convertUnixTimeToDate.bind(this);
         this.fetchData = this.fetchData.bind(this);
         this.createResults = this.createResults.bind(this);
-        this.createButtons = this.createButtons.bind(this);
     }
 
     convertUnixTimeToDate(unixTime) {
@@ -61,7 +56,7 @@ export default class pong extends React.Component {
                 }
                 resultViews.push(<Text
                               style={{
-                                        transform: [{translate: [1.2, 0, 0]}, {rotateY: -30}],
+                                        transform: [{translate: [1.2, 0.1, 0]}, {rotateY: -30}],
                                         backgroundColor: '#242424af',
                                         marginTop: 0.01,
                               }}>
@@ -74,46 +69,45 @@ export default class pong extends React.Component {
         return resultViews
     }
 
-        render() {
-                return (
-                        <View>
-                                <Pano source={asset('background.jpg')}/>
-                                <View style={{
-                                        transform: [{translate: [0, 0, -1]}],
-                                        width: 1,
-                                        layoutOrigin: [0.5, 0.5],
-                                }}>
-                                        <VrButton
-                                                style={{
-                                                        marginTop: 0.1
-                                                }}
-                                                onClick={()=>this.fetchData(1)}>
-                                                <Text
-                                                        style= {{
-                                                                textAlign: 'center',
-                                                                backgroundColor: this.state.buttons[0].color,
-                                                        }}>
-                                                        {this.state.buttons[0].stop}
-                                                </Text>
-                                        </VrButton>
-                                        <VrButton
-                                                style={{
-                                                        marginTop: 0.1
-                                                }}
-                                                onClick={()=>this.fetchData(69)}>
-                                                <Text
-                                                        style= {{
-                                                                textAlign: 'center',
-                                                                backgroundColor: this.state.buttons[1].color,
-                                                        }}>
-                                                        {this.state.buttons[1].stop}
-                                                </Text>
-                                        </VrButton>
-
-                                        {this.createResults()}
-                                </View>
-                        </View>
-                );
+    render() {
+        return (
+            <View>
+                <Pano source={asset('background.jpg')}/>
+                <View style={{
+                    transform: [{translate: [0, 0, -1]}],
+                    width: 1,
+                    layoutOrigin: [0.5, 0.5],
+                }}>
+                    <VrButton
+                        style={{
+                            marginTop: 0.1
+                        }}
+                        onClick={()=>this.fetchData(1)}>
+                        <Text
+                            style= {{
+                                textAlign: 'center',
+                                backgroundColor: "#904824af",
+                            }}>
+                            Pysäkki 1
+                        </Text>
+                    </VrButton>
+                    <VrButton
+                        style={{
+                            marginTop: 0.1
+                        }}
+                        onClick={()=>this.fetchData(69)}>
+                        <Text
+                            style= {{
+                                textAlign: 'center',
+                                backgroundColor: "#701024af",
+                            }}>
+                            Pysäkki 69
+                        </Text>
+                    </VrButton>
+                    {this.createResults()}
+                </View>
+            </View>
+        );
         }
 };
 
